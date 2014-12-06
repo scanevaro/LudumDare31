a7b1ae17412c3e938449192cf7569d87aef8364d
Kruhlmann <andreaskruhlmann@gmail.com>
2014-12-06 14:02:15 +0100

    lul

--- core/src/com/deeep/jam/World.java
+++ core/src/com/deeep/jam/World.java
@@ -1,25 +1,42 @@
 package com.deeep.jam;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.deeep.jam.entities.Blob;
 import com.deeep.jam.entities.Globe;
 import com.deeep.jam.screens.AbstractGame;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * Created by scanevaro on 05/12/2014.
  */
 public class World {
+
     public static Globe globe;
+    public static ArrayList<Blob> blobs;
 
     public World() {
         globe = new Globe();
+        blobs = new ArrayList<Blob>();
     }
 
     public void update(float deltaT){
         globe.update(deltaT);
+        ListIterator iT = blobs.listIterator();
+        while(iT.hasNext()){
+            Blob b = (Blob) iT.next();
+            b.update(deltaT);
+        }
     }
 
     public void draw(SpriteBatch spriteBatch){
         globe.draw(spriteBatch);
+        ListIterator iT = blobs.listIterator();
+        while(iT.hasNext()){
+            Blob b = (Blob) iT.next();
+            b.draw(spriteBatch);
+        }
     }
 
 }
