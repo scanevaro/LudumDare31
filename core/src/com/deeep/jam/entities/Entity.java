b26764ce98fdfa2a24296e48062dbd094c2c2939
E <elmarvanrijnswou@hotmail.com>
2014-12-06 14:06:50 +0100

    Entity

---
+++ core/src/com/deeep/jam/entities/Entity.java
@@ -0,0 +1,34 @@
+package com.deeep.jam.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by E on 12/6/2014.
+ */
+public class Entity {
+    protected Sprite sprite;
+    public float x, y;
+
+    public Entity() {
+        x = 0;
+        y = 0;
+    }
+
+    public Entity(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(float deltaT) {
+    }
+
+    public void draw(SpriteBatch spriteBatch) {
+        if (sprite != null) {
+            sprite.setPosition(x, y);
+            sprite.draw(spriteBatch);
+        }
+    }
+
+}
