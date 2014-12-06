0e46c0935b72192026e8da6e54cc22c812918e8d
Sebastián <scanevaro@hotmail.com>
2014-12-06 10:42:22 -0300

    GameInputListener

--- core/src/com/deeep/jam/screens/GameScreen.java
+++ core/src/com/deeep/jam/screens/GameScreen.java
@@ -36,6 +36,7 @@ public class GameScreen implements Screen {
 
     public void mouseMoved(int x, int y) {
         logger.system(Gdx.app.getApplicationListener().getClass(), "x = " + x + ", y = " + y);
+        //TODO globe movement
     }
 
     @Override
