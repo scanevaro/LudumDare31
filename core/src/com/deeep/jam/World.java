1501d4484869e4c9b0cdeff8740e92dc7b212037
Kruhlmann <andreaskruhlmann@gmail.com>
2014-12-06 14:43:24 +0100

    lul

--- core/src/com/deeep/jam/World.java
+++ core/src/com/deeep/jam/World.java
@@ -2,41 +2,34 @@ package com.deeep.jam;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.deeep.jam.entities.Blob;
+import com.deeep.jam.entities.BlobManager;
 import com.deeep.jam.entities.Globe;
+import com.deeep.jam.screens.AbstractGame;
 
 import java.util.ArrayList;
 import java.util.ListIterator;
 
 /**
- * Created by scanevaro on 05/12/2014.
+ * Created by Andreas on 05/12/2014.
  */
 public class World {
 
     public static Globe globe;
-    public static ArrayList<Blob> blobs;
+    public static BlobManager blobManager
 
     public World() {
         globe = new Globe();
-        blobs = new ArrayList<Blob>();
+        blobManager = new BlobManager();
     }
 
-    public void update(float deltaT) {
+    public void update(float deltaT){
         globe.update(deltaT);
-        ListIterator iT = blobs.listIterator();
-        while (iT.hasNext()) {
-            Blob b = (Blob) iT.next();
-            b.update(deltaT);
-        }
+        blobManager.update(deltaT);
     }
 
-    public void draw(SpriteBatch batch) {
-        batch.begin();
-        globe.draw(batch);
-        ListIterator iT = blobs.listIterator();
-        while (iT.hasNext()) {
-            Blob b = (Blob) iT.next();
-            b.draw(batch);
-        }
-        batch.end();
+    public void draw(SpriteBatch spriteBatch){
+        globe.draw(spriteBatch);
+        blobManager.draw(spriteBatch);
     }
+
 }
---
+++ core/src/com/deeep/jam/entities/Wave.java
@@ -0,0 +1,27 @@
+package com.deeep.jam.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.deeep.jam.screens.Core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Andreas on 12/6/2014.
+ */
+public class Wave {
+
+
+    public static ArrayList<Blob> blobs;
+
+    public Wave (){
+
+    }
+
+    public void update (float deltaT) {
+
+    }
+
+    public void draw (SpriteBatch spriteBatch) {
+    }
+}
