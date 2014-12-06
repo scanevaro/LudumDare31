4c38847ff3c6f851169198655552d17f846c5dd0
Sebastián <scanevaro@hotmail.com>
2014-12-05 21:30:00 -0300

    Initial commit

---
+++ html/src/com/deeep/jam/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.deeep.jam.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.deeep.jam.Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Game();
+        }
+}
