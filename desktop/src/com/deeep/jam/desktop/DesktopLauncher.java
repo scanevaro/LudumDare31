e9d22e4756e10399e1b438761dba5b4aa083ca93
Sebastián <scanevaro@hotmail.com>
2014-12-05 23:34:39 -0300

    clean

--- desktop/src/com/deeep/jam/desktop/DesktopLauncher.java
+++ desktop/src/com/deeep/jam/desktop/DesktopLauncher.java
@@ -2,12 +2,11 @@ package com.deeep.jam.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.deeep.jam.Game;
 import com.deeep.jam.screens.Core;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Core(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Core(), config);
+    }
 }
--- html/src/com/deeep/jam/client/HtmlLauncher.java
+++ html/src/com/deeep/jam/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.deeep.jam.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.deeep.jam.Game;
+import com.deeep.jam.screens.Core;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new Game();
+                return new Core();
         }
 }
