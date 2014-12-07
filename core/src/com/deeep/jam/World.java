6609490f23d6c751333852f8ce0a0778782d01be
Sebastián <scanevaro@hotmail.com>
2014-12-07 19:50:41 -0300

    game over

--- core/src/com/deeep/jam/World.java
+++ core/src/com/deeep/jam/World.java
@@ -9,17 +9,19 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.deeep.jam.background.Space;
 import com.deeep.jam.entities.*;
 import com.deeep.jam.input.Assets;
 import com.deeep.jam.screens.Core;
 import com.deeep.jam.screens.Menu;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Random;
-
-import static com.deeep.jam.PixmapRotater.getRotatedPixmap;
 
 /**
  * Created by Andreas on 05/12/2014.
@@ -50,11 +52,27 @@ public class World {
     private ArrayList<Circle> circles = new ArrayList<Circle>();
     private Menu menu;
 
+
+    public int state;
+    public static final int PLAYING = 0;
+    public static final int GAMEOVER = 1;
+
+    private Stage stage;
+    private TextButton[] gameOverListChar;
+    private TextButton[] scoreLabelChar;
+    private FloatArray post1, advances1, post2, advances2;
+    private String gamesOverText, scoreLabelText;
+    private static final float time = 0.5f, delay = 0.2f;
+
     /**
      * ༼ง ͠ຈ ͟ل͜ ͠ຈ༽ง gimme my memes ༼ง ͠ຈ ͟ل͜ ͠ຈ༽ง
      */
 
     public World() {
+        instantiate();
+    }
+
+    private void instantiate() {
         Assets.getAssets().loadBitmapFont();
         bitmapFont = Assets.getAssets().getBitmapFont();
         menu = new Menu(this);
@@ -74,7 +92,83 @@ public class World {
         space = new Space(500);
         //difficulty.spawn(globe, blobManager);
         roulette = new Roulette(this, globe);
+        globe.getGlobeImage().addRegion(difficulty.colors.get(0));
+        difficulty.colors.remove(0);
+        globe.getGlobeImage().addRegion(difficulty.colors.get(0));
+        difficulty.colors.remove(0);
+        globe.getGlobeImage().addRegion(difficulty.colors.get(0));
+        difficulty.colors.remove(0);
 
+        stage = new Stage(new StretchViewport(512, 512), Core.batch);
+        gamesOverText = "Game Over";
+        scoreLabelText = "Score: ";
+        gameOverListChar = new TextButton[gamesOverText.length()];
+        scoreLabelChar = new TextButton[scoreLabelText.length()];
+        advances1 = new FloatArray();
+        advances2 = new FloatArray();
+        post1 = new FloatArray();
+        post2 = new FloatArray();
+        Assets.getAssets().getBitmapFont().computeGlyphAdvancesAndPositions(gamesOverText, advances1, post1);
+        Assets.getAssets().getBitmapFont().computeGlyphAdvancesAndPositions(scoreLabelText, advances2, post2);
+
+        final TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.font = Assets.getAssets().getBitmapFont();
+
+        /** ヽ༼◕ل͜◕༽ﾉ */
+
+        for (int i = 0; i < gamesOverText.length(); i++) {
+            gameOverListChar[i] = new TextButton(String.valueOf(gamesOverText.charAt(i)), style);
+            gameOverListChar[i].setTransform(true);
+            gameOverListChar[i].setPosition(110, 360);
+//            gameOverListChar[i].setOrigin(advances1.get(i) / 2, gameOverListChar[i].getHeight() / 4);
+            stage.addActor(gameOverListChar[i]);
+        }
+
+        for (int i = 0; i < scoreLabelText.length(); i++) {
+            scoreLabelChar[i] = new TextButton(String.valueOf(scoreLabelText.charAt(i)), style);
+            scoreLabelChar[i].setTransform(true);
+            scoreLabelChar[i].setPosition(160, 360);
+            scoreLabelChar[i].setOrigin(advances2.get(i) / 2, scoreLabelChar[i].getHeight() / 4);
+            stage.addActor(scoreLabelChar[i]);
+        }
+
+        state = PLAYING;
+    }
+
+    private void resetText() {
+        for (int i = 0; i < gamesOverText.length(); i++) {
+            gameOverListChar[i].setPosition(110 + post1.get(i), 330);
+            gameOverListChar[i].setOrigin(advances1.get(i) / 2, gameOverListChar[i].getHeight() / 4);
+            gameOverListChar[i].setColor(0, 0, 0, 1);
+            gameOverListChar[i].setScale(1f);
+        }
+
+        for (int i = 0; i < scoreLabelText.length(); i++) {
+            scoreLabelChar[i].setPosition(170 + post2.get(i), 330);
+            scoreLabelChar[i].setOrigin(advances2.get(i) / 2, scoreLabelChar[i].getHeight() / 4);
+            scoreLabelChar[i].setColor(0, 0, 0, 1);
+            scoreLabelChar[i].setScale(1f);
+        }
+    }
+
+    private void prepareDrop() {
+        int tempX = 0;
+        for (int i = 0; i < gamesOverText.length(); i++) {
+            gameOverListChar[i].setY(330 + 200f);
+            gameOverListChar[i].setColor(0, 0, 0, 0);
+            gameOverListChar[i].addAction(Actions.delay(delay * i, Actions.parallel(Actions.alpha(1, time),
+                    Actions.moveTo(100 + post1.get(i) + tempX, 330, time, Interpolation.bounceOut))));
+            tempX += 15;
+        }
+
+        tempX = 0;
+        for (int i = 0; i < scoreLabelText.length(); i++) {
+            scoreLabelChar[i].setY(330 + 200f);
+            scoreLabelChar[i].setColor(0, 0, 0, 0);
+            scoreLabelChar[i].addAction(Actions.delay(delay * i, Actions.parallel(Actions.alpha(1, time),
+                    Actions.moveTo(150 + post2.get(i) + tempX, 200, time, Interpolation.bounceOut))));
+            tempX += 15;
+        }
     }
 
     public void shockwave() {
@@ -82,70 +176,83 @@ public class World {
     }
 
     public void update(float deltaT) {
-        //backgroundRotation = -globe.getAngleFacing();
-        //space.setRotation((float) Math.toDegrees(backgroundRotation));
-        space.update(deltaT);
-        //background.setRotation((float) Math.toDegrees(backgroundRotation));
-        // Gdx.input.setCursorImage(getRotatedPixmap(Assets.getAssets().getKappaPixmap(), (float) Math.toDegrees(getMouseAngle()) + 180F), 16, 16);
-        globe.update(deltaT);
-        blobManager.update(deltaT);
-        ArrayList<Circle> remove = new ArrayList<Circle>();
-        for (Circle circle : circles) {
-            circle.radius += deltaT * 50;
-            for (Blob blob : blobManager.blobs) {
-                if (circle.contains(blob.x, blob.y)) {
-                    blob.d = circle.radius;
-                }
-            }
-            if (circle.radius >= 200) {
-                remove.add(circle);
-            }
-        }
-        circles.removeAll(remove);
-        for (Blob blob : blobManager.blobs) {
-            if (!blob.isDead) {
-                float angle = (float) ((float) Math.atan2(blob.x - 256, blob.y - 256) + Math.PI / 2);
-                int distance = (int) blob.d;
-                Color color = globe.getColor(angle, distance);
-                if (color == null) {
-                    //do nothing not colliding
-                } else {
-                    if (color.r == blob.color.r && color.g == blob.color.g && color.b == blob.color.b) {
-                        difficulty.kill(globe, blobManager);
-                        Assets.getAssets().pointsGained.play();
-                        blob.die();
-                    } else {
-                        blob.die();
-                        difficulty.playerHit(globe, blobManager);
-                        damageTimer += 100;
-                        Assets.getAssets().incorrect.play();
+        switch (state) {
+            case PLAYING:
+                //backgroundRotation = -globe.getAngleFacing();
+                //space.setRotation((float) Math.toDegrees(backgroundRotation));
+                space.update(deltaT);
+                //background.setRotation((float) Math.toDegrees(backgroundRotation));
+                // Gdx.input.setCursorImage(getRotatedPixmap(Assets.getAssets().getKappaPixmap(), (float) Math.toDegrees(getMouseAngle()) + 180F), 16, 16);
+                globe.update(deltaT);
+                blobManager.update(deltaT);
+                ArrayList<Circle> remove = new ArrayList<Circle>();
+                for (Circle circle : circles) {
+                    circle.radius += deltaT * 50;
+                    for (Blob blob : blobManager.blobs) {
+                        if (circle.contains(blob.x, blob.y)) {
+                            blob.d = circle.radius;
+                        }
+                    }
+                    if (circle.radius >= 200) {
+                        remove.add(circle);
                     }
                 }
-            }
-        }
-        for (PowerBlob powerBlob : powerBlobManager.powerBlobs) {
-            if (!powerBlob.isDead) {
-                float adaptedX = Gdx.input.getX() - 24;
-                float adaptedY = 488 - Gdx.input.getY();
-                float dX = Math.abs(adaptedX - powerBlob.x);
-                float dY = Math.abs(adaptedY - powerBlob.y);
-                if (dX <= 40 && dY <= 40) {
-                    powerBlob.die();
-                    Assets.getAssets().power.play();
-                    roulette.newSession();
+                circles.removeAll(remove);
+                for (Blob blob : blobManager.blobs) {
+                    if (!blob.isDead) {
+                        float angle = (float) ((float) Math.atan2(blob.x - 256, blob.y - 256) + Math.PI / 2);
+                        int distance = (int) blob.d;
+                        Color color = globe.getColor(angle, distance);
+                        if (color == null) {
+                            //do nothing not colliding
+                        } else {
+                            if (color.r == blob.color.r && color.g == blob.color.g && color.b == blob.color.b) {
+                                difficulty.kill(globe, blobManager);
+                                Assets.getAssets().pointsGained.play();
+                                blob.die();
+                            } else {
+                                blob.die();
+                                difficulty.playerHit(globe, blobManager);
+                                damageTimer += 100;
+                                Assets.getAssets().incorrect.play();
+                            }
+                        }
+                    }
                 }
-            }
+                for (PowerBlob powerBlob : powerBlobManager.powerBlobs) {
+                    if (!powerBlob.isDead) {
+                        float adaptedX = Gdx.input.getX() - 24;
+                        float adaptedY = 488 - Gdx.input.getY();
+                        float dX = Math.abs(adaptedX - powerBlob.x);
+                        float dY = Math.abs(adaptedY - powerBlob.y);
+                        if (dX <= 40 && dY <= 40) {
+                            powerBlob.die();
+                            Assets.getAssets().power.play();
+                            roulette.newSession();
+                        }
+                    }
+                }
+                powerBlobManager.update(deltaT);
+                if (damageTimer >= 1000)
+                    gameOver();
+
+                difficulty.spawn(globe, blobManager);
+                break;
+            case GAMEOVER:
+                stage.act();
+
+                if (Gdx.input.justTouched())
+                    instantiate();
+
+                break;
         }
-        powerBlobManager.update(deltaT);
-        if (damageTimer >= 1000) {
-            gameOver();
-        }
-        if (!menu.show)
-            difficulty.spawn(globe, blobManager);
     }
 
     private void gameOver() {
-
+        state = GAMEOVER;
+        /** Prepare game over gamesOverText */
+        resetText();
+        prepareDrop();
     }
 
     public void draw(SpriteBatch batch) {
@@ -162,19 +269,24 @@ public class World {
         Gdx.gl.glDisable(GL20.GL_BLEND);
         batch.begin();
         globe.draw(batch);
-        roulette.draw(batch);
-        bitmapFont.setScale(1);
-        bitmapFont.draw(batch, "Score: " + difficulty.score, 10, 512 - 5);
-        int tempY = (int) bitmapFont.getLineHeight();
-        bitmapFont.setScale(0.5f);
-        bitmapFont.draw(batch, "Multiplier: " + difficulty.multiplier + "x", 10, 512 - 10 - tempY + bitmapFont.getLineHeight());
-        bitmapFont.setScale(0.4f);
-        bitmapFont.draw(batch, "" + difficulty.consecutive, 512 - 25, 512 - 25);
-        if (globe.color != null) {
-            bitmapFont.draw(batch, "r: " + globe.color.r, 10, 15);
-            bitmapFont.draw(batch, "g: " + globe.color.g, 10, 15 + bitmapFont.getLineHeight());
-            bitmapFont.draw(batch, "b: " + globe.color.b, 10, 15 + +bitmapFont.getLineHeight() + bitmapFont.getLineHeight());
+
+        if (state == PLAYING) {
+            roulette.draw(batch);
+
+            bitmapFont.setScale(1);
+            bitmapFont.draw(batch, "Score: " + difficulty.score, 10, 512 - 5);
+            int tempY = (int) bitmapFont.getLineHeight();
+            bitmapFont.setScale(0.5f);
+            bitmapFont.draw(batch, "Multiplier: " + difficulty.multiplier + "x", 10, 512 - 10 - tempY + bitmapFont.getLineHeight());
+            bitmapFont.setScale(0.4f);
+            bitmapFont.draw(batch, "" + difficulty.consecutive, 512 - 25, 512 - 25);
+            if (globe.color != null) {
+                bitmapFont.draw(batch, "r: " + globe.color.r, 10, 15);
+                bitmapFont.draw(batch, "g: " + globe.color.g, 10, 15 + bitmapFont.getLineHeight());
+                bitmapFont.draw(batch, "b: " + globe.color.b, 10, 15 + +bitmapFont.getLineHeight() + bitmapFont.getLineHeight());
+            }
         }
+
         batch.end();
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
@@ -217,7 +329,16 @@ public class World {
             explosionOverlay.draw(batch);
         }
         menu.draw(batch);
+
+        if (state == GAMEOVER) {
+            bitmapFont.setScale(1.5f);
+            bitmapFont.draw(batch, "" + difficulty.score, 200, 150);
+        }
+
         batch.end();
+
+        if (state == GAMEOVER)
+            stage.draw();
     }
 
     /**
